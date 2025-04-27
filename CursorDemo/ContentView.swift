13877bc7cf0b40c045349d809c2521a7c5953d49
huaiyuanzheng <1410342773@qq.com>
2025-04-27 16:56:04 +0800

    Initial Commit

---
+++ CursorDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursorDemo
+//
+//  Created by Huaiyuan Zheng on 2025/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursorDemo/CursorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorDemoApp.swift
+//  CursorDemo
+//
+//  Created by Huaiyuan Zheng on 2025/4/27.
+//
+
+import SwiftUI
+
+@main
+struct CursorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
